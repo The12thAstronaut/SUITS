b01d61e01bc7e76f35eae9dfaa03644fba952cbf
georgecoltstegg <georgecoltstegg@tamu.edu>
2019-11-03 22:31:34 -0600

    Readable JSON Coordinates to move waypoint

--- MoonVR/Assets/Scripts/MovingWaypoint.cs
+++ MoonVR/Assets/Scripts/MovingWaypoint.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using LitJson;
 
 public class MovingWaypoint : MonoBehaviour
 {
     string jsonString;
+    JsonData itemData;
     Vector3 TempPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        jsonString = File.ReadAllText(Application.dataPath + "/JSONFiles/HjsonData.json"); //read all data into jsonString
-        HjsonData Suit = JsonUtility.FromJson<HjsonData>(jsonString); //put all data into variable suit
+        jsonString = File.ReadAllText(Application.dataPath + "/JSONFiles/HjsonData.json"); //Adds all text from json file to a string
+        itemData = JsonMapper.ToObject(jsonString);
 
-        // transform position of waypoint based on the json data values for x,y,z
         TempPos = transform.position;
 
-        TempPos.x = Suit.x;
-        TempPos.y = Suit.y;
-        TempPos.z = Suit.z;
+        int json_x = (int) itemData["x"];
+        int json_y = (int) itemData["y"];
+        int json_z = (int) itemData["z"];
+
+        TempPos.x = json_x;
+        TempPos.y = json_y;
+        TempPos.z = json_z;
+
+        transform.position = TempPos;
     }
 
     // Update is called once per frame
