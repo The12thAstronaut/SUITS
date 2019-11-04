6b1a6a86969a34c2e08ac9432ac635a809dd6390
dkirbs <54824885+dkirbs@users.noreply.github.com>
2019-11-03 20:50:02 -0600

    PGT/Bolt integration

---
+++ MoonVR/Assets/ScrewInBolt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewInBolt : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "PGT")
+        {
+            transform.Translate(0, 0, speed);
+        }
+    }
+}
