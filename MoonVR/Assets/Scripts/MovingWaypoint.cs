bb560f2cd06b7b04780cd87a0ea3716f850c1ae3
georgecoltstegg <georgecoltstegg@tamu.edu>
2019-11-04 19:43:54 -0600

    Edited waypoint code

--- MoonVR/Assets/Scripts/MovingWaypoint.cs
+++ MoonVR/Assets/Scripts/MovingWaypoint.cs
@@ -16,16 +16,19 @@ public class MovingWaypoint : MonoBehaviour
         jsonString = File.ReadAllText(Application.dataPath + "/JSONFiles/HjsonData.json"); //Adds all text from json file to a string
         itemData = JsonMapper.ToObject(jsonString);
 
-        TempPos = transform.position;
+        TempPos = transform.position; // set vector variable as the x,y,z position of the object
 
+        // set variables for the x,y,z data coming from the JSON file
         int json_x = (int) itemData["x"];
         int json_y = (int) itemData["y"];
         int json_z = (int) itemData["z"];
 
+        // change the current x,y,z values to the new x,y,z values
         TempPos.x = json_x;
         TempPos.y = json_y;
         TempPos.z = json_z;
 
+        // finally change the current position to the new position
         transform.position = TempPos;
     }
 
