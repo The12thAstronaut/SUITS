5381be9aa56d712a2b1ef498c86a0074c2858692
georgecoltstegg <georgecoltstegg@tamu.edu>
2019-11-12 00:49:37 -0600

    Added Scripts to move panel up and down when clciking buttons

---
+++ MoonVR/Assets/Scripts/DoorClose.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorClose : MonoBehaviour
+{
+    public GameObject Panel;
+    public bool panelIsClosing;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (panelIsClosing == true)
+        {
+            Panel.transform.Translate(Vector3.down * Time.deltaTime * 5);
+        }
+        if (Panel.transform.position.y < 1.2f)
+        {
+            panelIsClosing = false;
+        }
+    }
+    void OnMouseDown() // should detect when clicking on collider with mouse
+    {
+        panelIsClosing = true;
+    }
+}
---
+++ MoonVR/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public GameObject Panel;
+    public bool panelIsOpening;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (panelIsOpening==true)
+        {
+            Panel.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+        if (Panel.transform.position.y > 2f)
+        {
+            panelIsOpening = false;
+        }
+    }
+    void OnMouseDown() // should detect when clicking on collider with mouse
+    {
+        panelIsOpening = true;
+    }
+}
