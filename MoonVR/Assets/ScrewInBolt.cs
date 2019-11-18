ea814a7eeb6c194418af13ff5189889bd2f9296b
dkirbs <54824885+dkirbs@users.noreply.github.com>
2019-11-18 11:22:30 -0600

    Update ScrewInBolt.cs

--- MoonVR/Assets/ScrewInBolt.cs
+++ MoonVR/Assets/ScrewInBolt.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ScrewInBolt : MonoBehaviour
 {
     public float speed = 0.1f;
+    bool bolt_in = false;
+    bool bolt_out = false;
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +16,34 @@ public class ScrewInBolt : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnCollisionEnter(Collision col)
+    void Update()
     {
-        if(col.gameObject.tag == "PGT")
+
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            float m_distanceTraveled = 0f;
-                if (m_distanceTraveled < 100f)
-                {
-                    Vector3 oldPosition = transform.position;
-                    transform.Translate(0, 0, 1 * Time.deltaTime);
-                    m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
-                }
-          
+            bolt_in = true;
         }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            bolt_out = true;
+        }
+
+        if (bolt_in == true)
+        {
+               transform.Translate(0f, -1f * Time.deltaTime, 0f);
+        }
+        if (transform.position.z < 2.92f)
+        {
+                bolt_in = false;
+        }
+        if (bolt_out == true)
+        {
+            transform.Translate(0f, 1f * Time.deltaTime, 0f);
+        }
+        if (transform.position.z > 3.2f)
+        {
+            bolt_out = false;
+        }
+
     }
 }
