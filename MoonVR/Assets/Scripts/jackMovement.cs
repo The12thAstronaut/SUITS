9c9406242d79d34e51fb5b8ba53ed789fbb4c890
georgecoltstegg <georgecoltstegg@tamu.edu>
2019-11-25 22:21:32 -0600

    Added Jack Button with Up and Down Functionality

--- MoonVR/Assets/Scripts/jackMovement.cs
+++ MoonVR/Assets/Scripts/jackMovement.cs
@@ -2,32 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorButton : MonoBehaviour
+public class jackMovement : MonoBehaviour
 {
-    public GameObject Door;
-    public bool DoorOpen;
-    public bool DoorClose;
+    public GameObject Jack;
+    public bool JackUp;
+    public bool JackDown;
     public Camera MainCamera;
 
     // Update is called once per frame
     void Update()
     {
         OnMouseOver();
-        if (DoorOpen == true)
+        if (JackUp == true)
         {
-            Door.transform.Translate(Vector3.up * Time.deltaTime * 1);
+            Jack.transform.Translate(Vector3.up * Time.deltaTime * 1);
         }
-        if (Door.transform.position.y > 2f)
+        if (Jack.transform.position.y > 1f)
         {
-            DoorOpen = false;
+            JackUp = false;
         }
-        if (DoorClose == true)
+        if (JackDown == true)
         {
-            Door.transform.Translate(Vector3.down * Time.deltaTime * 1);
+            Jack.transform.Translate(Vector3.down * Time.deltaTime * 1);
         }
-        if (Door.transform.position.y < 1.1f)
+        if (Jack.transform.position.y < 0.575f)
         {
-            DoorClose = false;
+            JackDown = false;
         }
     }
     void OnMouseOver()
@@ -40,18 +40,18 @@ public class DoorButton : MonoBehaviour
             //If the hit object tag is "OpenButton", then change panelIsOpening to true
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider.tag == "OpenButton")
+                if (hit.collider.tag == "JackButton")
                 {
                     //hit.collider.gameObject now refers to the 
                     //cube under the mouse cursor if present
                     Debug.Log("Door Open Button Clicked");
-                    if (Door.transform.position.y < 2)
+                    if (Jack.transform.position.y < 1)
                     {
-                        DoorOpen = true;
+                        JackUp = true;
                     }
-                    if (Door.transform.position.y >= 2)
+                    if (Jack.transform.position.y >= 1)
                     {
-                        DoorClose = true;
+                        JackDown = true;
                     }
                 }
             }
