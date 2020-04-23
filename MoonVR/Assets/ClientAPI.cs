9dd55ce5c0514a34a40dfacf07a91f68bedef834
dkirbs <54824885+dkirbs@users.noreply.github.com>
2020-04-22 19:16:15 -0500

    Telem update

--- MoonVR/Assets/ClientAPI.cs
+++ MoonVR/Assets/ClientAPI.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class ClientApi : MonoBehaviour
+public class ClientAPI : MonoBehaviour
 {
     public string url;
 
@@ -27,7 +27,23 @@ public class ClientApi : MonoBehaviour
                 {
                     // handle the result
                     var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
-                    Debug.Log(result);
+                    var data = result[0];
+
+                    Debug.Log(data);
+
+                    var suit = JsonUtility.FromJson<Suit>(result);
+
+                    Debug.Log("Heart rate is " + suit.bpm);
+                    Debug.Log("Pressure sub is " + suit.psub);
+                    Debug.Log("Suit Pressure is " + suit.psuit);
+                    Debug.Log("Temperature Sub is " + suit.tsub);
+                    Debug.Log("Velocity of fan is " + suit.vfan);
+                    Debug.Log("Rate of oxygen is " + suit.rox);
+                    Debug.Log("Battery Capacity is " + suit.batcap);
+                    Debug.Log("Pressure of gaseous water is " + suit.gaseous);
+                    Debug.Log("Pressure of liquid water is " + suit.liquid);
+                    Debug.Log("Pressure of sop is " + suit.gaseous);
+
                 }
                 else
                 {
@@ -37,5 +53,43 @@ public class ClientApi : MonoBehaviour
             }
         }
 
+    }
+    public IEnumerator Post(string url, Suit suit)
+    {
+        var jsonData = JsonUtility.ToJson(suit);
+        Debug.Log(jsonData);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, jsonData))
+        {
+            www.SetRequestHeader("content-type", "application/json");
+            www.uploadHandler.contentType = "application/json";
+            www.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(jsonData));
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                if (www.isDone)
+                {
+                    // handle the result
+                    var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                    result = "{\"result\":" + result + "}";
+                    var resultData = JsonHelper.FromJson<Suit>(result);
+
+                    foreach (var item in resultData)
+                    {
+                        Debug.Log(item.bpm);
+                    }
+                }
+                else
+                {
+                    //handle the problem
+                    Debug.Log("Error! data couldn't get.");
+                }
+            }
+        }
     }
 }
---
+++ MoonVR/Assets/Suit.cs
@@ -0,0 +1,20 @@
+﻿
+[System.Serializable]
+public class Suit
+{
+    public string bpm;
+    public string psub;
+    public string psuit;
+    public string tsub;
+    public string vfan;
+    public string pox;
+    public string rox;
+    public string batcap;
+    public string gaseous;
+    public string liquid;
+    public string psop;
+    public string batlife;
+    public string oxlife;
+    public string waterlife;
+    public string time;
+}
