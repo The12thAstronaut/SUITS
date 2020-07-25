1a018d99ecc9583fdfa35983d0757b4c352c61bd
Neil McHenry <neil.g.mchenry@nasa.gov>
2020-07-25 18:58:59 -0500

    Set KeyCode.E to Toggle PGT

--- MoonVR/Assets/Scripts/AnimationLogic/Toolbelt.cs
+++ MoonVR/Assets/Scripts/AnimationLogic/Toolbelt.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Toolbelt : MonoBehaviour
 {
+
+    //Make the PGT variable public so the PGT game object can be attached through the Unity Editor
+    public GameObject PGT;
+    bool toggleBool = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +18,20 @@ public class Toolbelt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            print("space key was pressed");
+            //Toggle the PGT game object on and off if the keyboard E button is pressed
+            toggleBool = !toggleBool;
+            PGT.SetActive(toggleBool);
+
+            if(toggleBool)
+            {            
+                print("PGT toggled OFF");
+            }
+            else
+            {
+            print("PGT toggled ON");
+            }
         }    
     }
 }
