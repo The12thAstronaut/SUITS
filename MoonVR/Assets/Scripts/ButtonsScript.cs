fa213b65f6da3afc8b862b068571b995fe77f90b
mannycarr214 <43549928+mannycarr214@users.noreply.github.com>
2020-08-13 14:57:06 -0500

    Mannys First Commit
    I have added the Panel and all of its buttons are under its own hirearchy so they can be accessed.
    
    The first 2 buttons on the top-left of the screen have the script that I am currently working on to add more immersion. So far they change the material color on the cube that will in the future power a light.

---
+++ MoonVR/Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsScript : MonoBehaviour
+{
+    public Material mat;
+    public GameObject cube;
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            cube.GetComponent<Renderer>().material = mat;
+        }
+    }
+}
