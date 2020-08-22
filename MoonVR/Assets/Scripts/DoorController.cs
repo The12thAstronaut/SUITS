fe248f09cba60627248f4d29e12fb9c98caba234
IzzyG <47159819+IzzyGomezThree@users.noreply.github.com>
2020-08-22 03:24:52 -0500

    Israel's Technical Test's
    Issues:
    1. Panel Opening- https://www.youtube.com/watch?v=nONlAXpCkag
    I can't get the panel to open using "F" on keyboard (note, pressing "F" is only allowed when inside box collider near buttons
    2. Raycasting - https://www.youtube.com/watch?v=_yf5vzZ2sYE&vl=en
    The ray isn't allined with the center & objects with Tag "Selectable" are suppose to be highlighted when glanced at, but aren't

---
+++ MoonVR/Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public GameObject instructions;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Panel")
+        {
+            instructions.SetActive(true);
+            Animator anim ;
+            anim = other.GetComponent<Animator>();
+            if(Input.GetKeyDown("1"))
+            {
+                anim.SetTrigger("Open/Close");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Panel")
+        {
+            instructions.SetActive(false);
+        }
+    }
+
+}
