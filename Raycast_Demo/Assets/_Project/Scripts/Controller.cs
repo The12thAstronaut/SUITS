b72072946e4ebc455a374b7261c8bed70e58113f
IzzyG <47159819+IzzyGomezThree@users.noreply.github.com>
2020-08-23 18:18:57 -0500

    Raycast Demo Trial

---
+++ Raycast_Demo/Assets/_Project/Scripts/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject instructions_button1;
+    public GameObject instructions_button2;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Button")
+        {
+            instructions_button1.SetActive(true);
+            //instructions_button2.SetActive(true);
+        }
+    }
+    
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Button")
+        {
+            instructions_button1.SetActive(false);
+            //instructions_button2.SetActive(false);
+        }
+    }
+    
+}
